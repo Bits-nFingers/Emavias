1972231e771c7d13f05488cce1fd21c33ca3d3f9
SergiusxD <sbellott@hotmail.com>
2019-12-05 08:56:49 -0400

    Errores

--- app/src/main/java/com/example/emavias/MainActivity.java
+++ app/src/main/java/com/example/emavias/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.emavias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button Ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Ingresar = findViewById(R.id.Ingresar);
+        Ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,Login.class));
+                finish();
+            }
+        });
     }
 }

8af25c4ca7a47f9fe30f395e264e5b48deddbcc2
SergiusxD <sbellott@hotmail.com>
2019-12-10 20:02:22 -0400

    Agregando todos los layouts de nuestra aplicacion

--- app/src/main/java/com/example/emavias/Login.java
+++ app/src/main/java/com/example/emavias/Login.java
@@ -2,13 +2,28 @@ package com.example.emavias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
+    Button iniciar_sesion;
+
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        iniciar_sesion  = findViewById(R.id.iniciar_sesion);
+        iniciar_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Opciones.class));
+                finish();
+            }
+        });
     }
 }

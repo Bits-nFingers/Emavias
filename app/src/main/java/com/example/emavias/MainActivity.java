75363dcc939f1c80f5ea70c2377273160bf583d1
PaoRivas <paoa.rivasm@gmail.com>
2019-12-11 08:41:18 -0400

    Merge branch 'master' of C:\Users\ASUS\AndroidStudioProjects\EMAVIAS with conflicts.

--- app/src/main/java/com/example/emavias/MainActivity.java
+++ app/src/main/java/com/example/emavias/MainActivity.java
@@ -2,27 +2,29 @@ package com.example.emavias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
-import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button Ingresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*Runnable mRunnable;
+        Handler mHandler=new Handler();
 
-        Ingresar = findViewById(R.id.Ingresar);
-        Ingresar.setOnClickListener(new View.OnClickListener() {
+        mRunnable=new Runnable() {
+
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,Login.class));
-                finish();
+            public void run() {
+                // TODO Auto-generated method stub
+                LinearLayout.setVisibility(View.GONE); //This will remove the View. and free s the space occupied by the View
             }
-        });
+        };
+
+        mHandler.postDelayed(mRunnable,10*1000);*/
     }
 }

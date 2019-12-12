ffcde692403111baac288a1a562747cc879264f1
PaoRivas <paoa.rivasm@gmail.com>
2019-12-11 21:59:53 -0400

    Hice la conexión Android con el Servidor Rest

--- app/src/main/java/com/example/emavias/MainActivity.java
+++ app/src/main/java/com/example/emavias/MainActivity.java
@@ -4,8 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +38,28 @@ public class MainActivity extends AppCompatActivity {
         };
 
         mHandler.postDelayed(mRunnable,10*1000);*/
+        String URL = "http://localhost/jsbcscmlkmisn";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("Rest Response", response.toString());
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Rest Response", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(objectRequest);
+
     }
 }

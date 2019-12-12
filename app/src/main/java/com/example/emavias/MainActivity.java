c589f494b90734fc475ee6a9515e9d4e35f1bf4d
PaoRivas <paoa.rivasm@gmail.com>
2019-12-12 07:57:51 -0400

    test

--- app/src/main/java/com/example/emavias/MainActivity.java
+++ app/src/main/java/com/example/emavias/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Login.class));
+
             }
         });
 
